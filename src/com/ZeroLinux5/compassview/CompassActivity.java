ad201aad2cd754cf36bdffc784cba05ac7d1ba68
Jesus Magana <maganazero5@gmail.com>
2014-09-14 11:27:20 -0700

    Added view override class to make own view

---
+++ src/com/ZeroLinux5/compassview/CompassActivity.java
@@ -0,0 +1,56 @@
+package com.ZeroLinux5.compassview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class CompassActivity extends View{
+
+	public CompassActivity(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+		initCompassView();
+	}
+	
+	public CompassActivity(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		initCompassView();
+	}
+	
+	public CompassActivity(Context context) {
+		super(context);
+		initCompassView();
+	}
+	
+	protected void initCompassView() { 
+		setFocusable(true);
+	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		// The compass is a circle that fills as much space as possible.
+		// Set the measured dimensions by figuring out the shortest boundary,
+		// height or width.
+		int measuredWidth = measure(widthMeasureSpec);
+		int measuredHeight = measure(heightMeasureSpec);
+		
+		int d = Math.min(measuredWidth, measuredHeight);
+		
+		setMeasuredDimension(d, d);
+	}
+	
+	private int measure(int measureSpec) { 
+		int result = 0;
+		// Decode the measurement specifications.
+		int specMode = MeasureSpec.getMode(measureSpec); 
+		int specSize = MeasureSpec.getSize(measureSpec);
+		if (specMode == MeasureSpec.UNSPECIFIED) {
+			// Return a default size of 200 if no bounds are specified. 
+			result = 200;
+		} else {
+			// As you want to fill the available space 
+			// always return the full available bounds. 
+			result = specSize;
+		}
+		return result; 
+	}
+}
