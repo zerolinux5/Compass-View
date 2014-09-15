4bb2d6ed22457fe950f0270f548335024cad186b
Jesus Magana <maganazero5@gmail.com>
2014-09-14 17:22:00 -0700

    Added logic

--- gen/com/ZeroLinux5/compassview/R.java
+++ gen/com/ZeroLinux5/compassview/R.java
@@ -1231,6 +1231,10 @@ containing a value of this type.
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int app_name=0x7f0a000d;
+        public static final int cardinal_east=0x7f0a0010;
+        public static final int cardinal_north=0x7f0a000f;
+        public static final int cardinal_south=0x7f0a0011;
+        public static final int cardinal_west=0x7f0a0012;
         public static final int hello_world=0x7f0a000e;
     }
     public static final class style {
--- src/com/ZeroLinux5/compassview/CompassActivity.java
+++ src/com/ZeroLinux5/compassview/CompassActivity.java
@@ -1,14 +1,28 @@
 package com.ZeroLinux5.compassview;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.accessibility.AccessibilityEvent;
 
 public class CompassActivity extends View{
 	private float bearing;
 	
+	private Paint markerPaint; 
+	private Paint textPaint; 
+	private Paint circlePaint; 
+	private String northString; 
+	private String eastString; 
+	private String southString; 
+	private String westString; 
+	private int textHeight;
+	
 	public void setBearing(float _bearing) { 
 		bearing = _bearing;
+		sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED);
 	}
 	
 	public float getBearing() { 
@@ -32,6 +46,21 @@ public class CompassActivity extends View{
 	
 	protected void initCompassView() { 
 		setFocusable(true);
+		
+		Resources r = this.getResources();
+		circlePaint = new Paint(Paint.ANTI_ALIAS_FLAG); 
+		circlePaint.setColor(r.getColor(R.color.background_color)); 
+		circlePaint.setStrokeWidth(1); 
+		circlePaint.setStyle(Paint.Style.FILL_AND_STROKE);
+		northString = r.getString(R.string.cardinal_north); 
+		eastString = r.getString(R.string.cardinal_east); 
+		southString = r.getString(R.string.cardinal_south); 
+		westString = r.getString(R.string.cardinal_west);
+		textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+		textPaint.setColor(r.getColor(R.color.text_color)); 
+		textHeight = (int)textPaint.measureText("yY");
+		markerPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+		markerPaint.setColor(r.getColor(R.color.marker_color));
 	}
 
 	@Override
@@ -61,5 +90,75 @@ public class CompassActivity extends View{
 			result = specSize;
 		}
 		return result; 
+	}
+	
+	@Override
+	protected void onDraw(Canvas canvas) {
+		int mMeasuredWidth = getMeasuredWidth(); 
+		int mMeasuredHeight = getMeasuredHeight();
+		int px = mMeasuredWidth / 2; 
+		int py = mMeasuredHeight / 2 ;
+		int radius = Math.min(px, py);
+		
+		// Draw the background
+		canvas.drawCircle(px, py, radius, circlePaint);
+		
+		// Rotate our perspective so that the 'top' is 
+		// facing the current bearing.
+		canvas.save();
+		canvas.rotate(-bearing, px, py);
+		
+		int textWidth = (int)textPaint.measureText("W"); 
+		int cardinalX = px-textWidth/2;
+		int cardinalY = py-radius+textHeight;
+		// Draw the marker every 15 degrees and text every 45. 
+		for (int i = 0; i < 24; i++) {
+			// Draw a marker.
+			canvas.drawLine(px, py-radius, px, py-radius+10, markerPaint);
+			
+			canvas.save();
+			
+			canvas.translate(0, textHeight);
+			// Draw the cardinal points 
+			if (i % 6 == 0) {
+				String dirString = "";
+				switch(i){
+					case(0): {
+						dirString = northString;
+						int arrowY = 2*textHeight;
+						canvas.drawLine(px, arrowY, px-5, 3*textHeight, markerPaint); 
+						canvas.drawLine(px, arrowY, px+5, 3*textHeight, markerPaint);
+						break;
+					}
+					case(6): dirString = eastString; break;
+					case(12): dirString = southString; break;
+					case(18): dirString = westString; break;
+				}
+				canvas.drawText(dirString, cardinalX, cardinalY, textPaint);
+			}else if (i % 3 == 0) {
+				// Draw the text every alternate 45deg
+				String angle = String.valueOf(i*15);
+				float angleTextWidth = textPaint.measureText(angle);
+				int angleTextX = (int)(px-angleTextWidth/2);
+				int angleTextY = py-radius+textHeight; canvas.drawText(angle, angleTextX, angleTextY, textPaint);
+				} canvas.restore();
+				canvas.rotate(15, px, py); 
+			}
+			canvas.restore(); 
+		}
+	
+	@Override
+	public boolean dispatchPopulateAccessibilityEvent(final AccessibilityEvent event) {
+		super.dispatchPopulateAccessibilityEvent(event);
+		if (isShown()) {
+			String bearingStr = String.valueOf(bearing);
+			if (bearingStr.length() > AccessibilityEvent.MAX_TEXT_LENGTH)
+				bearingStr = bearingStr.substring(0, AccessibilityEvent.MAX_TEXT_LENGTH);
+			
+			event.getText().add(bearingStr);
+			return true; 
+		} else {
+			return false;
+		}
 	}
 }
